2280832dfd7013ad6a728e3d3bd40d8b9fef4d6b
navot-moveo <navot@moveo.co.il>
2019-12-01 08:18:51 +0200

    jwt testing

--- api/validators/authValidator.js
+++ api/validators/authValidator.js
@@ -5,6 +5,8 @@ const secretKey = config.get('jwt.secretKey');
 
 //this mathod verify users token
 function verifyToken(req, res, next){
+    console.log("verify token " + req.headers['token'])
+    console.log("secretKey " + secretKey)
     jwt.verify(req.headers['token'], secretKey, (err, authData) => {
         if(err){
             next(err);
